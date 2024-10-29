6768119adfcc4993a0700c41d024dfc02aa87807
leex17 <xinyi.lee@bsci.com>
2024-10-29 14:17:21 +0800

    V2_10 Improved logs

--- CopyV2_10.bat
+++ CopyV2_10.bat
@@ -35,7 +35,7 @@ for /d %%d in (%sourceDir%\*) do (
 
     :: Copy files from the source subfolder to the newly created date folder in the destination subfolder
     :: Duplicates are not copied
-    xcopy %sourceDir%\"!subfolder!"\* %destinationDir%\"!subfolder!"\%folderName% /s /e /d
+    xcopy %sourceDir%\"!subfolder!"\* %destinationDir%\"!subfolder!"\%folderName% /s /e /d /q
 )
 endlocal
 

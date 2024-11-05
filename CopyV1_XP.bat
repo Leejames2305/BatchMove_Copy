2e306a19bdd30efbbd7fdce68088b0ce0e13403e
leex17 <xinyi.lee@bsci.com>
2024-11-05 13:48:57 +0800

    V1_XP Updated xcopy switch to overwrite

--- CopyV1_XP.bat
+++ CopyV1_XP.bat
@@ -22,7 +22,7 @@ if not exist  "%datePath%" (
 )
 
 :: Copy files from source to newly created folder in destination, duplicates are not copied, unless they are newer
-xcopy "%sourceDir%" "%datePath%" /e /d /q
+xcopy "%sourceDir%" "%datePath%" /e /d /q /y
 
 :: Pause the script to see the output
 pause

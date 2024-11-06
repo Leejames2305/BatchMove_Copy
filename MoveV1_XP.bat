ebb9ac2cae0bd258de525e93485d815add98ed03
leex17 <xinyi.lee@bsci.com>
2024-11-06 16:12:15 +0800

    MoveV1 Initial

---
+++ MoveV1_XP.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal enabledelayedexpansion
+:: Automated script that runs daily in task scheduler, will move files from one directory to another.
+:: In the destination directory, it will split the date into detailed folders, and move files into.
+:: (SourceDir\29Dec24\Stuff.xyz) -> (DestinationDir\2024\12\29\Stuff.xyz)
+
+:: Set the source and destination directories
+set "sourceDir=F:\"
+set "destinationDir=\\vmware-host\Shared Folders\tempshared\"
+
+:: Get the date from the folders' name
+for /d %%d in (%sourceDir%\*) do (
+    set folderName=%%~nxd
+    set year=20!folderName:~5,2!
+    set month=!folderName:~2,3!
+    set day=!folderName:~0,2!
+    echo Year: !year! Month: !month! Day: !day!
+    
+    REM Create the folder with today's date in the destination directory
+    set "datePath=%destinationDir%\!year!\!month!\!day!"
+
+    if not exist "!datePath!" (
+        mkdir "!datePath!"
+    )
+
+    REM Move files from source to newly created folder in destination
+    xcopy "%%d\*" "!datePath!" /e /d /q /y
+
+    REM Remove the source folder
+    if !errorlevel! equ 0 (
+        echo Copy success, deleting folder: %%d
+        rmdir "%%d" /s /q
+    ) else (
+        echo Copy failed, not deleting folder: %%d
+    )
+)
+endlocal
+
+:: Pause the script to see the output
+pause
